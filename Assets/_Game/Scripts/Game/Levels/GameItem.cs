5afff05a016da72b8546ce8b5770bcf158247291
Po <>
2026-01-18 17:40:03 +0700

    [features] update level demo

--- Assets/_Game/Scripts/Game/Levels/GameItem.cs
+++ Assets/_Game/Scripts/Game/Levels/GameItem.cs
@@ -5,10 +5,8 @@ namespace SpotDifferences
     public class GameItem : MonoBehaviour
     {
         [SerializeField] private string _id;
-        [SerializeField] private int _sceneId;
 
         public string Id => _id;
-        public int SceneId => _sceneId;
 
         public void SetActive(bool isActive)
         {
